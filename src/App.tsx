0f5ebecb7d0394a5e1b0df98c26c00c3423f4e5f
Salina Tulik <salina.tulik@gmail.com>
2024-10-23 13:18:50 -0600

    create DashboardPage and add route for /dashboard to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./Pages/LandingPage/LandingPage.tsx";
 import SignupPage from "./Pages/SignupPage/SignupPage.tsx";
 import LoginPage from "./Pages/LoginPage/LoginPage.tsx";
+import DashboardPage from "./Pages/DashboardPage/DashboardPage.tsx";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<LandingPage />} />
           <Route path="/signup" element={<SignupPage />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
         </Routes>
       </BrowserRouter>{" "}
     </>
